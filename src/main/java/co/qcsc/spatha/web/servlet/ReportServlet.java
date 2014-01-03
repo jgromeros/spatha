862837bd82066a161f4e428b963cf7d566a49c71
juano <jgromero@gmail.com>
2014-01-02 21:21:34 -0500

    - Obteniendo conexión desde dataSource

--- src/main/java/co/qcsc/spatha/web/servlet/ReportServlet.java
+++ src/main/java/co/qcsc/spatha/web/servlet/ReportServlet.java
@@ -16,7 +16,13 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperRunManager;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ReportServlet extends HttpServlet {
+
+    @Autowired
+    public BasicDataSource dataSource;
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -40,32 +46,16 @@ public class ReportServlet extends HttpServlet {
     public void generateReport(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Connection connection;
-
         InputStream reportStream =getServletConfig().getServletContext().getResourceAsStream(
                 "/resources/reports/index.jasper");
         ServletOutputStream servletOutputStream = response.getOutputStream();
-
-        // Nos conectamos a la base de datos (creamos una coneccion)
         try {
-            Class.forName("org.postgresql.Driver");
-            // Ojo mybase es el nombre de la base, user y password.
-//            connection = DriverManager.getConnection(
-//                    "jdbc:postgresql://localhost:5432/sid", "uvlab",
-//                    "uvlab");
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://ec2-107-20-245-187.compute-1.amazonaws.com:5432/d98sb9cb44b3uf",
-                    "wxprdqdkfalprs", "iisCdwiwVHa1gF2c5h2fddF3jR");
-            // seteamos el contentType
+            connection = dataSource.getConnection();
             response.setContentType("application/pdf");
 
-            // ejecutamos el reporte
             JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, new HashMap(),
                     connection);
-            // Cerramos la coneccion a la Base
             connection.close();
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
