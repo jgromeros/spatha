7e369e80eb0d4c7960b9dbf155f9c2cd84f73a63
jorgemontes <jamaff86@gmail.com>
2014-01-07 21:07:13 -0500

    Converter de Product

---
+++ src/main/java/co/qcsc/spatha/web/mb/converter/ProductConverter.java
@@ -0,0 +1,9 @@
+package co.qcsc.spatha.web.mb.converter;
+
+import org.springframework.roo.addon.jsf.converter.RooJsfConverter;
+
+import co.qcsc.spatha.domain.product.Product;
+
+@RooJsfConverter(entity = Product.class)
+public class ProductConverter {
+}
